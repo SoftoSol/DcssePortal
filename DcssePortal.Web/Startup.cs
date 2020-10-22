b347dd53efdfa844f16585ccd527558f25b586cd
arham-anees <arhamanees10@gmail.com>
2020-10-23 02:39:22 +0500

    InitialCommit

---
+++ DcssePortal.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DcssePortal.Web.Startup))]
+namespace DcssePortal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
