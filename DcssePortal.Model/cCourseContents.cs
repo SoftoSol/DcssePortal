63b436bb809336edf49568419458080178a466fe
ahmad-raz <raza12386@gmail.com>
2020-10-24 20:50:15 +0500

    ModelsPartiallyAdded

---
+++ DcssePortal.Model/cCourseContents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcssePortal.Model
+{
+    [Table("tCourseContents")]
+    public class cCourseContents
+    {
+        [Key]
+        public int ID { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseData { get; set; }
+        public string CourseTitle { get; set; }
+        
+
+    }
+}
---
+++ DcssePortal.Model/cStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcssePortal.Model
+{
+    [Table("tStudent")]
+    public class cStudent
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public string Age { get; set;}
+        public string Email { get; set;}
+        public string Phone { get; set;}
+        public string Profile { get; set;}
+        public DateTime DOB { get; set;}
+        public string Password { get; set; }
+
+    }
+}
